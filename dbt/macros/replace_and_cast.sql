85d9d0103f979e05aa0d5efd20d8f940b6c8e0f9
Markus Lehtola <markus.lehtola@zure.com>
2025-01-03 08:41:33 +0000

    Add documentation to macro

--- dbt/macros/replace_and_cast.sql
+++ dbt/macros/replace_and_cast.sql
@@ -1,4 +1,16 @@
 {% macro replace_and_cast(column, null_value, data_type) %}
+    {# 
+    This macro replaces specific values in a column with NULL and casts the result to a specified data type. 
+    It also handles cases where numeric values may include commas as thousand separators.
+
+    Parameters:
+    - column (str): The column name to apply the transformation.
+    - null_value (str): The value to be treated as NULL. Example: 'unknown' or an empty string ''.
+    - data_type (str): The target data type for casting. Example: 'decimal(16,3)', 'int', 'date', etc.
+
+    Usage:
+    {{ replace_and_cast('[population]', "'unknown'", 'decimal(16,3)') }}
+    #}
     case 
         when {{ column }} = '{{ null_value }}' then NULL
         else cast(replace({{ column }}, ',', '') as {{ data_type }})
